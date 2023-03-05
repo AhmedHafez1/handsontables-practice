df0a6c2c867f69b3c432b5728134bc32b7f8fd62
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2023-03-05 08:27:14 +0200

    Created HoldToDeleteDirective

--- src/app/hold-to-delete.directive.ts
+++ src/app/hold-to-delete.directive.ts
@@ -1,10 +1,33 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostListener, Output } from '@angular/core';
+import { interval, takeWhile } from 'rxjs';
 
 @Directive({
-  selector: '[appHoldToDelete]'
+  selector: '[holdToDelete]',
 })
 export class HoldToDeleteDirective {
+  private holding = false;
+  private progressPercent: number = 0;
 
-  constructor() { }
+  @Output() holdProgress: EventEmitter<number> = new EventEmitter();
 
+  @HostListener('mousedown') startHoldToDelete() {
+    this.holding = true;
+
+    const holdToDeleteObservable = interval(50).pipe(
+      takeWhile(() => this.holding)
+    );
+  }
+
+  @HostListener('mouseup') endHoldToDelete() {
+    this.holding = false;
+  }
+
+  progressPercentage() {
+    if (this.progressPercent < 100) {
+      this.progressPercent += 2;
+    }
+    if (this.progressPercent > 100) {
+      this.progressPercent = 100;
+    }
+  }
 }
